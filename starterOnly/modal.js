b38a75ce3258038bff2580dd7d56a245a08479f3
Valentin <ottvalentin1@gmail.com>
2021-08-05 09:37:33 +0200

    Fermeture du Formulaire JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// CLOSE MODAL FORM
+function closeModal() {
+  modalbg.style.display = "none";
+}
+closeBtn[0].addEventListener('click', closeModal);
 
 
