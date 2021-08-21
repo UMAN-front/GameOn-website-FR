64cdc790406cadd4ae210f865f27c0b30ffea3bc
Valentin <ottvalentin1@gmail.com>
2021-08-21 22:52:41 +0200

    Reset inscription et padding desktop main

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -3,7 +3,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName('close');
-
 // ------ DISPLAY MODAL ------ //
 // LAUNCH MODAL EVENTS
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -14,8 +13,32 @@ function launchModal() {
 // CLOSE MODAL FORM
 function closeModal() {
   modalbg.style.display = 'none';
+  first.parentElement.setAttribute('data-error-visible', 'false');
+  first.style.border = 'solid #279e7a 0rem';
+  
+  last.parentElement.setAttribute('data-error-visible', 'false');
+  last.style.border = 'solid #279e7a 0rem';
+
+  email.parentElement.setAttribute('data-error-visible', 'false');
+  email.style.border = 'solid #279e7a 0rem';
+
+  birthdate.parentElement.setAttribute('data-error-visible', 'false');
+  birthdate.style.border = 'solid #279e7a 0rem';
+  
+  quantity.parentElement.setAttribute('data-error-visible', 'false');
+  quantity.style.border = 'solid #279e7a 0rem';
+
+  checkbox1.parentElement.setAttribute('data-error-visible', 'false');
+
+  allLocations.setAttribute('data-error-visible', 'false');
 }
 closeBtn[0].addEventListener('click', closeModal);
+rest.forEach((formData) => parentElement.addEventListener('change', reset));
+
+function reset() {
+console.log("Bonjour");
+
+}
 
 // ------ DISPLAY NAV RESPONSIVE ------ //
 // EDIT NAV
@@ -27,5 +50,3 @@ function editNav() {
     x.className = 'topnav';
   }
 }
-
-
